75d2917d34d677cad009100706cf0e6fc76ffb90
CapBarbosssa <97461923+CapBarbosssa@users.noreply.github.com>
2022-06-26 22:23:33 -0600

    small change

--- react-mini/src/components/Navbar.jsx
+++ react-mini/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import  React,{useState} from 'react';
+
 import styled from "styled-components";
 import { Search } from "@material-ui/icons";
 import { Badge, Link } from "@mui/material";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MenuItem from '@mui/material/MenuItem';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import Divider from '@mui/material/Divider';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 
 const Container = styled.div`
   height: 10px;
@@ -53,7 +62,16 @@ const Menuitem = styled.div`
   margin-left: 30px;
   text-decoration: none;
 `;
+
 function Navbar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div>
       <Container>
@@ -81,9 +99,83 @@ function Navbar() {
               <Search style={{ color: "gray", fontSize: 15 }} />
             </SearchContainer>
 
-            <Menuitem><Link href={"/login"} underline="none"color={"black"}>
+            <Menuitem>
+            <Link href={"/login"} underline="none"color={"black"}>
                 Sign In
-              </Link></Menuitem>
+              </Link>
+               {/* <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 30, height: 30 }}>User</Avatar>
+          </IconButton>
+          <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu> */}
+              
+              </Menuitem>
             <Menuitem>
               <Badge badgeContent={2} color="primary">
                 <ShoppingCartOutlinedIcon />
